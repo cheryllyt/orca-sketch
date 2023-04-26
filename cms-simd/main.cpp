a88eca6b573f3482b9aa3d48e9f403901ce10ab2
cheryllyt <yan.lai.20@ucl.ac.uk>
2023-04-26 05:37:30 +0100

    clean main.cpp file comments

--- cms-simd/main.cpp
+++ cms-simd/main.cpp
@@ -11,25 +11,6 @@
 using namespace std;
 
 /*
-    no SIMD version
-    ===============
-
-    data structure:
-    SUMMARY - one row of counters, split into arrays
-    let W = total number of counters (in the row)
-    let B = size of a array (in number of counters)
-    therefore, number of arrays = W / B
-    let A = number of counters an item should be mapped into within a array
-    therefore, number of options for which A counters within a array an item should be mapped into
-             = number of rows in lookup table (of counter combinations)
-             = B choose A (combination) 
-
-    algorithm:
-    1. Use BobHash (the hash function) on an item -> returns 32 bits
-    2. Split into 22 bits and 10 (least significant) bits
-    3. Use the 10 bits to map into a array
-    4. Use the 22 bits to map into a row in the lookup table
-
     command to compile on MacBook:
     ------------------------------
     g++ -std=c++17 -mavx2 -O3 main.cpp ORCAS.cpp ORCASTests.cpp salsa-src/BobHash.cpp -framework Python -DDEBUG
